f6d2627163bbfdd58b0d7df3e959a89f552df28a
Камиль Сайфуллин <kamilsayfullin@mail.ru>
2025-12-24 04:57:44 +0300

    sorry

--- app/util.py
+++
@@ -1,5 +0,0 @@
-def get_paginator_range(paginator_length: int, current_page: int, total_pages: int):
-    half_paginator = paginator_length // 2
-    start = max(1, current_page - half_paginator)
-    end = min(total_pages, max(current_page + half_paginator, paginator_length))
-    return range(start, end + 1)
